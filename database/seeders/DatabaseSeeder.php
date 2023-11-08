f7b8658252f57961c004a4c6915e6e1aa469482b
VhrsHACK <vihirmujahid8@gmail.com>
2023-11-08 13:20:29 +0700

    Push Terbaru

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Prodi;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,23 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        Prodi::create(
+            [
+                "nama"=> "Teknik Informatika",
+            ]
+        );
+
+        Prodi::create(
+            [
+                "nama"=> "Manajemen Informatika",
+            ]
+        );
+
+        Prodi::create(
+            [
+                "nama"=> "Sistem Informasi",
+            ]
+        );
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\ProdiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,97 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route ke halaman profile
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+//Route dengan parameter (Wajib)
+// Route::get('/mahasiswa/{nama}', function ($nama = "Ardi") {
+//     echo "<h1>Halo Nama Saya $nama</h2>";
+// });
+
+//Route dengan parameter (Tidak Wajib)
+Route::get('/mahasiswa2/{nama?}', function ($nama = "Ghifari") {
+    echo "<h1>Halo Nama Saya $nama</h2>";
+});
+
+//Route dengan parameter lebih dari 1
+Route::get(
+    '/profile/{nama?}/{pekerjaan?}',
+    function ($nama = "Ardi", $pekerjaan = "Mahasiswa") {
+        echo "<h1>Halo Nama Saya $nama, Saya adalah $pekerjaan</h2>";
+    }
+);
+
+//Route redirect
+Route::get('/hubungi', function () {
+    echo "<h2>Hubungi Kami</h2>";
+})->name("call");
+
+Route::redirect('/contact', '/hubungi');
+
+//Route dengan nama
+Route::get('/halo', function () {
+    echo "<a href='" . route('call') . "'>" . route('call') . "</a>";
+});
+
+//Route Group
+Route::prefix("/dosen")->group(function () {
+    Route::get("/jadwal", function () {
+        echo "<h1>Jadwal Dosen </h1>";
+    });
+    Route::get("/materi", function () {
+        echo "<h1>Materi Perkuliahan</h1>";
+    });
+    //dan lain - lain
+});
+
+//Route Dosen
+Route::get('/dosen', function () {
+    return view('dosen');
+});
+
+//Route Dosen index
+Route::get('/dosen/index', function () {
+    return view('dosen.index');
+});
+
+//Route Fakultas
+Route::get('/fakultas', function () {
+    // return view('fakultas.index', ["ilkom" => ["Fakultas Ilmu Komputer
+    // dan Rekayasa"]);
+
+    // return view('fakultas.index', ["fakultas" => ["Fakultas Ilmu Komputer
+    // dan Rekayasa", "Fakultas Ilmu Ekonomi"]
+    // ]);
+
+    // return view('fakultas.index')->with("fakultas",["Fakultas Ilmu Komputer
+    // dan Rekayasa","Fakultas Ilmu Ekonomi"]);
+
+    // $fakultas = ["Fakultas Ilmu Komputer dan Rekayasa", "Fakultas Ilmu Ekonomi"];
+    // return view ('fakultas.index',compact('fakultas'));
+
+    $kampus = "Universitas Multi Data Palembang";
+    $fakultas = ["Fakultas Ilmu Komputer dan Rekayasa", "Fakultas Ilmu Ekonomi"];
+    return view('fakultas.index', compact('fakultas', 'kampus'));
+});
+
+Route::get('/mahasiswa/insert', [MahasiswaController::class, 'insert']);
+Route::get('/mahasiswa/update', [MahasiswaController::class, 'update']);
+Route::get('/mahasiswa/delete', [MahasiswaController::class, 'delete']);
+Route::get('/mahasiswa/select', [MahasiswaController::class, 'select']);
+
+Route::get('/mahasiswa/insert-qb', [MahasiswaController::class, 'insertQb']);
+Route::get('/mahasiswa/update-qb', [MahasiswaController::class, 'updateQb']);
+Route::get('/mahasiswa/delete-qb', [MahasiswaController::class, 'deleteQb']);
+Route::get('/mahasiswa/select-qb', [MahasiswaController::class, 'selectQb']);
+
+Route::get('/mahasiswa/insert-elq', [MahasiswaController::class, 'insertElq']);
+Route::get('/mahasiswa/update-elq', [MahasiswaController::class, 'updateElq']);
+Route::get('/mahasiswa/delete-elq', [MahasiswaController::class, 'deleteElq']);
+Route::get('/mahasiswa/select-elq', [MahasiswaController::class, 'selectElq']);
+
+Route::post('/prodi/store', [ProdiController::class,'store']);
+Route::get('/prodi/create', [ProdiController::class,'create']);
